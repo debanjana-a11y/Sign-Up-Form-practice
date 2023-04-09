58a37f9031b4d1adc052450892dd907f498fb416
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2023-04-09 22:24:33 +0530

    Adds frontend

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+			backgroundImage: {
+				bgImage: "url('/images/bg.jpg')"
+			}
+		}
+  },
+  plugins: [],
+}
+
