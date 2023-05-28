b563d38392d9ba2ec97bee85ab502d77fa1b8759
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2023-05-28 19:54:30 +0530

    test 3

--- js/home.js
+++ js/home.js
@@ -1,12 +1,15 @@
+const initialPath = location.pathname;
+const loginPath = initialPath + '/login';
+
 const userName = sessionStorage.name;
 let greeting = document.getElementById('greeting');
-// if (userName === undefined) location.href = '/login';
+if (userName === undefined) location.href = loginPath;
 greeting.innerHTML = 'Konnichiwa ' + userName;
 
 const logoutBtn = document.getElementById('logout');
 if (logoutBtn !== null) {
     logoutBtn.addEventListener('click', () => {
-        location.href = '/login';
+        location.href = loginPath;
     });
 };
 
